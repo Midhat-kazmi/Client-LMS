052e54039796f1eeac065f1fb5250b9a66fafd76
Midhat-dot <midhatcodes@gmail.com>
2025-12-30 03:28:15 +0500

    fix in courses component

--- app/components/Route/Courses.tsx
+++ app/components/Route/Courses.tsx
@@ -8,25 +8,25 @@ const Courses = () => {
     { refetchOnMountOrArgChange: true }
   );
   const [courses, setCourses] = useState<any[]>([]);
+
   useEffect(() => {
     refetch();
-    setCourses(data?.course);
-  }, [data,refetch]);
+    if (data?.courses) setCourses(data.courses); 
+  }, [data, refetch]);
+
   return (
     <div>
-      <div className={`w-[90%] 800px:w-[80%] m-auto`}>
-        <h1 className="text-center font-Poppins text-[25px] leading-[35px] sm:text-3xl lg:text-4xl dark:text-white 800px:!leading-[60px] text-black font-bold tracking-tight">
+      <div className="w-[90%] 800px:w-[80%] m-auto">
+        <h1 className="text-center font-Poppins text-[25px] leading-[35px] sm:text-3xl lg:text-4xl dark:text-white text-black font-bold tracking-tight">
           Expand Your Career <span className="text-gradient">Opportunity</span>
           <br />
           Opportunity With Our Courses
         </h1>
-        <br />
-        <br />
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
-          {courses &&
-            courses.map((item: any, index: number) => (
-              <CourseCard item={item} key={index} />
-            ))}
+
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 1500px:grid-cols-4 mb-12 mt-10">
+          {courses.map((item: any, index: number) => (
+            <CourseCard item={item} key={index} />
+          ))}
         </div>
       </div>
     </div>
